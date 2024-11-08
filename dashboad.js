ad3a5cddcab425dda493b28f581f180c189dcdf9
DHRUVGANGAL <dhruvgangal123@gmail.com>
2024-11-08 20:34:14 +0530

    hey

--- dashboad.js
+++ dashboad.js
@@ -1,20 +1,28 @@
-const main=document.getElementsByClassName("main")
-        const val=document.getElementById("sorted").value 
-        console.log(val)
+// import user_name from "./message.js";
+// const text1=document.getElementById(name);
+// text1.innerText=`Welcome ${importedValue}`;
+
+
+const main=document.getElementById("main");
+const val=document.getElementById("sorted").value ;
+console.log(val)
 
         async function displayUsers(){
         const data=await fetch("https://api.github.com/users")
-        const rest=await data.json()
+        
+        const rest=await data.json();
+        console.log(rest);
         let res=[]
         for(let i=0;i<11;i++){
             res[i]=rest[i]
         }
-        let list=document.createElement("ul")
-        let item=document.createElement("li")
+        let list=document.createElement('ul');
+        // let item=document.createElement('li');
         res.map((data)=>{
-            let item=document.createElement("li")
-            item.innerHTML=`<a>${data.login}</a>`
-            list.appendChild(item)
+            let item=document.createElement('li');
+            item.innerHTML= `<a href="${data.html_url}">${data.login}</a>`;
+            console.log(item);
+            list.appendChild(item);
         })
         main.appendChild(list)
         }
--- login.js
+++ login.js
@@ -1,15 +1,18 @@
+
 function submitHandler(event) {
     event.preventDefault();
-
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
-
+    
+ 
     if (username === "admin" && password === "admin") {
         window.location.href = "dashboad.html";
     } 
     else {
         alert("Invalid username or password");
     }
+    
+   
 }
 
 document.querySelector('.login-form').onsubmit = submitHandler;
