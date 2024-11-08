72567be68421bfa724f28d1045f6c57a79a4bc07
DHRUVGANGAL <dhruvgangal123@gmail.com>
2024-11-08 10:03:08 +0530

    hey

--- login.js
+++ login.js
@@ -1,9 +1,24 @@
-let username=document.getElementById(username).value;
-let password=document.getElementById(password).value;
+// let username=document.getElementById(username).value;
+// let password=document.getElementById(password).value;
 
 
-function login(){
-    if(username==="admin"&&password==="admin"){
-       
+// function login(){
+//     if(username==="admin"&&password==="admin"){
+//         window.open('dashboad.html', '_blank');
+//     }
+// }
+
+function submitHandler(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username === "admin" && password === "admin") {
+        window.location.href = "dashboard.html";
+    } else {
+        alert("Invalid username or password");
     }
 }
+
+document.querySelector('.login-form').onsubmit = submitHandler;
