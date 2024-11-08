0d5af2ec5f059c4c27bd24e29f6f9e2821b3d32c
DHRUVGANGAL <dhruvgangal123@gmail.com>
2024-11-08 09:52:42 +0530

    hey

---
+++ login.js
@@ -0,0 +1,9 @@
+let username=document.getElementById(username).value;
+let password=document.getElementById(password).value;
+
+
+function login(){
+    if(username==="admin"&&password==="admin"){
+       
+    }
+}
